be4262b66bec0fa99d1d0582dc99addbac189209
Kacper Konecki <kk@mobostudio.com>
2021-12-20 01:48:59 +0100

    todo

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -5,6 +5,13 @@
 <!--      <a-entity ></a-entity>-->
       <a-entity light="type: ambient" position="1 1 -4"></a-entity>
       <a-entity light="type: directional" position="5 30 5"></a-entity>
+<!--
+todo do files like keyboard, input user name like keyboard
+https://stemkoski.github.io/A-Frame-Examples/
+https://stemkoski.github.io/A-Frame-Examples/keyboard.html-->
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"
+             event-set__enter="_event: mouseenter; color: #8FF7FF"
+             event-set__leave="_event: mouseleave; color: #4CC3D9"></a-box>
       <a-entity position="0 0 5">
         <a-camera wasd-controls-enabled=true fov=60 near=0.1 far=10000>
           <a-cursor></a-cursor>
